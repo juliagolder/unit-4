196bdb757f4f16da58b41fbe7f84d623d04dbd4d
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-22 08:23:26 -0400

    Updated from Brython Server: 3/22/2018 8:23:26 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -12,6 +12,15 @@ CELL_SIZE = 20
 def moveRight(event):
     monkey.x += CELL_SIZE
 
+def moveLeft(event):
+    monkey.x -= CELL_SIZE
+    
+def moveUp(event):
+    monkey.y -= CELL_SIZE
+    
+def moveDown(event):
+    monkey.y += CELL_SIZE
+
 if __name__ == '__main__':
 
     #colors
@@ -26,6 +35,9 @@ if __name__ == '__main__':
     monkey = Sprite(monkeyBox)
     
     App().listenKeyEvent('keydown', 'right arrow',moveRight)
+    App().listenKeyEvent('keydown', 'left arrow',moveLeft)
+    App().listenKeyEvent('keydown', 'down arrow',moveDown)
+    App().listenKeyEvent('keydown', 'up arrow',moveUp)
     
     App().run()
     
