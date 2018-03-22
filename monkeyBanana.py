86a9e2e082250505cc70285a11947fbaae8a693e
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-22 08:12:07 -0400

    Updated from Brython Server: 3/22/2018 8:12:07 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     #colors
     green = Color(0x006600, 1)
     
-    jungleBox = RectangleAsset(CELL_SIZE*COLS, CELL_SIE*ROWS, LineStyle(1,green),green)
+    jungleBox = RectangleAsset(CELL_SIZE*COLS, CELL_SIZE*ROWS, LineStyle(1,green),green)
     
     Sprite(jungleBox)
     
