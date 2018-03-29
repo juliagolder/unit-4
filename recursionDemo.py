66ab974da0ba404b518ccd0a522a88f3701c834c
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-29 08:09:30 -0400

    Updated from Brython Server: 3/29/2018 8:09:30 AM

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,1 +1,13 @@
+#julia golder
+#3/29/18
+#recursionDemo.py - recursive version of countdown
 
+def countdownr(n):
+    if n == 0: #base case
+        print('BOOM!')
+    else: #recursive step
+        print(n)
+        countdownr(n-1)
+
+countdownr(5)
+countdownr(15)
