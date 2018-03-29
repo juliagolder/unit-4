d513d4d92f6a4fcca2a67a9e7608624219738139
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-29 08:37:54 -0400

    Updated from Brython Server: 3/29/2018 8:37:54 AM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,15 @@
+#julia golder
+#3/29/18
+#warmup12.py
 
+
+def GCF(num1,num2):
+    i = num1
+    while i <= 1:
+        if num1%i == 0 and num2%i == 0:
+            return i
+        i+=1
+        
+print(GCF(12,15))
+print(GCF(5,9))
+print(GCF(16,64))
