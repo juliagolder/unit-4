55520e9713d0e63277066127c8278470042b3cfd
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-01 14:27:48 -0400

    Updated from Brython Server: 4/1/2018 2:27:49 PM

--- rectangle.py
+++ rectangle.py
@@ -2,8 +2,9 @@
 #3/9/18
 #rectangle.py
 
-def rectangle(a*b):
-    print(a*b)
+def rectangle(a,b):
+    print('Area =', a*b)
+    print('Perimeter =', (a*2) + (b*2)) 
 
 def perimeter(a+a+b+b):
     print(a+a+b+b)
