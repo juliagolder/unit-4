70753f983326191d2866dded55a86bc839ed2799
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-01 14:59:45 -0400

    Updated from Brython Server: 4/1/2018 2:59:44 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,13 @@
+#julia golder
+#4/1/18
+#stringUnion.py
 
+def stringIntersect(word1,word2):
+    answer = ''
+    for ch in word1:
+        if ch in word2 and ch not in answer:
+            answer = answer + ch
+    return(answer)
+
+
+print(stringIntersect('mississippi', 'pennsylvania'))
