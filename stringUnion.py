3a540ab3ca74d11d60dbf0d499c26c5e4cb555b8
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-01 14:43:34 -0400

    Updated from Brython Server: 4/1/2018 2:43:36 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,12 @@
+#julia golder
+#4/1/18
+#stringUnion.py
 
+def union(w1,w2):
+    answer = ''
+    for ch in w1 + w2:
+        if ch not in answer:
+            answer = answer + ch
+    retun(Answer)
+
+print(union('Mississippi', 'Pennsylvania'))
