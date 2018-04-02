661cf1af928b24b7789c2f7369ac83cb060c14db
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-04-02 08:51:09 -0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,31 @@
+#julia golder
+#4/2/18
+#quiz4.py
 
+
+def count(x):
+    for i in range(1,x+1):
+        print(i)
+    print('BOOM!')
+
+
+def excitedPrint(word):
+    print(word.upper() + '!!!')
+    
+def firstLetter(word2): 
+    letter = ''
+    for ch in word2:
+        if ch not in letter:
+            letter = letter + ch
+        return(letter)
+        
+def repeats(x,y,z):
+    if x == y or x == z or y == z:
+        return(True)
+    else:
+        return(False)
+
+count(7)
+excitedPrint('I <3 computer programming')
+print(firstLetter('Smedinghoff'))
+print(repeats(5,6,5))
